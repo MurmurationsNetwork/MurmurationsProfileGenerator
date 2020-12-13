30805a447fb3e8933ac72782c9341f3b74179b07
Geoff Turk <geoff@ic3.dev>
2020-12-13 01:01:15 +0100

    Set up first part of post profile step

--- components/ProfilePostSchema.js
+++ components/ProfilePostSchema.js
@@ -1,14 +1,54 @@
-import { Button, Text } from '@chakra-ui/react'
+import { Button, Input, Text } from '@chakra-ui/react'
+import { useState } from 'react'
 
 export default function ProfilePostSchema({ profile, setProfile }) {
   console.log('ProfilePostSchema/profile', profile)
+  const [submitted, setSubmitted] = useState(false)
+  const [posted, setPosted] = useState(false)
+  const [profileUrl, setProfileUrl] = useState('')
+  let profileJson = profile.json
+
+  profileJson.linked_schemas = profile.schemas
+
+  function handleInput(e) {
+    setSubmitted(false)
+    setProfileUrl(e.target.value)
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    setSubmitted(true)
+    if (profileUrl.length > 1) setPosted(true)
+  }
 
   return (
     <div>
       <Text>Post Schema</Text>
-      <Button m={1} onClick={() => setProfile({ ...profile, step: 2 })}>
-        Back
-      </Button>
+      {posted === true ? (
+        <Text>
+          The profile at {`<${profileUrl}>`} has been sent to the index for validation and posting.
+        </Text>
+      ) : (
+        <div>
+          <Button m={1} onClick={() => setProfile({ ...profile, step: 2 })}>
+            Back
+          </Button>
+          <Button m={1} onClick={handleSubmit}>
+            Next
+          </Button>
+          <Input
+            name="profileUrl"
+            type="text"
+            placeholder="example: https://your.site/subdirectory/profile.json"
+            value={profileUrl}
+            onChange={handleInput}
+          />
+          {submitted === true && profileUrl.length < 1 ? (
+            <Text>You need to enter a profile URL!</Text>
+          ) : null}
+          <Text as="pre">{JSON.stringify(profileJson, null, 2)}</Text>
+        </div>
+      )}
     </div>
   )
 }
