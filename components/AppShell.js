095d82f89ab5b93cb1c233e9036f79d976888416
Geoff Turk <geoff@ic3.dev>
2021-01-09 06:57:58 +0100

    Add demo message

--- components/AppShell.js
+++ components/AppShell.js
@@ -1,10 +1,18 @@
-import { Button, useColorMode } from '@chakra-ui/react'
+import { Button, Text, useColorMode } from '@chakra-ui/react'
 
 export default function AppShell({ children }) {
   const { colorMode, toggleColorMode } = useColorMode()
 
   return (
     <>
+      <Text align="center" color="tomato">
+        Please note that this is a TEST/DEMO only. Any data posted here to Murmurations will
+        eventually be wiped (probably in early February 2021 if not before).
+      </Text>
+      <Text align="center" color="tomato">
+        Don't post any information you would not be comfortable revealing on a public website
+        accessible to the world.
+      </Text>
       <Button onClick={toggleColorMode}>{colorMode === 'light' ? '🌙' : '☀️'}</Button>
       {children}
     </>
