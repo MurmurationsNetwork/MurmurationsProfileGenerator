e29fcd6f5e71f33d97167d27d2ab8dae6616df49
Geoff Turk <geoff@ic3.dev>
2021-01-14 22:05:21 +0100

    WIP

--- pages/ui/index.js
+++ pages/ui/index.js
@@ -2,24 +2,17 @@ import React from 'react'
 import { Box, Flex, Stack, Link, Button, Image, Heading, Text } from '@chakra-ui/react'
 
 const App = () => (
-  <Flex flexDirection="column" fontSize={['sm', 'sm', 'md', 'lg', 'xl']}>
+  <Flex flexDirection="column" fontSize={['xs', 'sm', 'md', 'lg', 'xl']}>
     <Flex
       display={{ base: 'grid', md: 'flex' }}
       flexDirection={{ base: 'column', md: 'row' }}
       backgroundColor="#efeeed"
       justifyContent="space-around"
       alignItems="center"
-      pt={{ base: 1, md: 4 }}
-      pb={{ base: 1, md: 4 }}
-      pl={{ base: 2, md: 16 }}
-      pr={{ base: 2, md: 16 }}
+      px={{ base: 2, md: 16 }}
+      py={{ base: 1, md: 4 }}
     >
-      <Flex
-        ml={{ base: 'auto', md: 0 }}
-        mr={{ base: 'auto', md: 0 }}
-        mt={{ base: 2, md: 0 }}
-        mb={{ base: 4, md: 0 }}
-      >
+      <Flex mx={{ base: 'auto', md: 0 }} my={{ base: 4, md: 0 }}>
         <Image
           height={['38px', '57px', '77px']}
           width={['50px', '75px', '100px']}
@@ -32,17 +25,17 @@ const App = () => (
         <Button
           variant="solid"
           size="md"
-          fontSize={['sm', 'sm', 'md', 'lg', 'xl']}
+          fontSize={['xs', 'sm', 'md', 'lg', 'xl']}
           backgroundColor="#f95a58"
           color="#ffffff"
-          borderRadius="15px"
-          height={[6, 8, 10]}
+          borderRadius="25px"
+          height={[6, 7, 8, 10]}
         >
           New Profile
         </Button>
       </Stack>
     </Flex>
-    <Flex backgroundColor="#ffffff" p={[2, 4, 8, 0]}>
+    <Flex backgroundColor="#ffffff" px={[4, 8, 16, 0]}>
       <Flex ml="auto" mr="auto" width="100%" maxWidth="780px" flexDirection="column">
         <Heading textAlign="center" textStyle="h2" mt={{ base: 4, md: 12 }}>
           Murmurations
