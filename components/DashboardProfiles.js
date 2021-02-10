dd1483c4c48f74fb5f0566fa556c7a75eed0aa56
Geoff Turk <geoff@ic3.dev>
2021-02-10 16:30:56 +0100

    WIP

--- components/DashboardProfiles.js
+++ components/DashboardProfiles.js
@@ -1,9 +1,9 @@
 import {
   Accordion,
-  AccordionItem,
   AccordionButton,
-  AccordionPanel,
   AccordionIcon,
+  AccordionItem,
+  AccordionPanel,
   Box,
   Button,
   Flex,
@@ -15,6 +15,14 @@ import {
   ModalFooter,
   ModalHeader,
   ModalOverlay,
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
   Text,
   useBreakpointValue,
   useDisclosure,
@@ -306,7 +314,40 @@ export default function DashboardProfiles({ profiles, setProfile }) {
             alignItems="center"
             width="100%"
           >
-            Desktop!
+            {/* <Table variant="unstyled">
+              <TableCaption>Imperial to metric conversion factors</TableCaption>
+              <Thead>
+                <Tr>
+                  <Th>To convert</Th>
+                  <Th>into</Th>
+                  <Th isNumeric>multiply by</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                <Tr>
+                  <Td>inches</Td>
+                  <Td>millimetres (mm)</Td>
+                  <Td isNumeric>25.4</Td>
+                </Tr>
+                <Tr>
+                  <Td>feet</Td>
+                  <Td>centimetres (cm)</Td>
+                  <Td isNumeric>30.48</Td>
+                </Tr>
+                <Tr>
+                  <Td>yards</Td>
+                  <Td>metres (m)</Td>
+                  <Td isNumeric>0.91444</Td>
+                </Tr>
+              </Tbody>
+              <Tfoot>
+                <Tr>
+                  <Th>To convert</Th>
+                  <Th>into</Th>
+                  <Th isNumeric>multiply by</Th>
+                </Tr>
+              </Tfoot>
+            </Table> */}
           </Flex>
         )
       ) : (
